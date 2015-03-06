10219f771137fbfa9ea5db5d9f7583aaafabf197
Ayal Pierce <ayalzev@gmail.com>
2015-03-05 19:11:14 -0500

    implemented Lab 6 Part 1

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,31 @@
-// Your JavaScript goes here...
+/*
+ * Ayal Pierce
+ * Tufts Comp 20: Web Programmign
+ * Spring 2015
+ * Lab 6: JSON
+*/
+
+function parse(){
+	request = new XMLHttpRequest();
+
+	request.onreadystatechange = parseData;	
+
+	request.open("GET", "data.json", true);
+
+	request.send();
+	
+}
+
+function parseData(){
+
+	if(request.readyState == 4){
+		messagesDiv = document.getElementById("messages");
+		converted = JSON.parse(request.responseText);
+		for (i = 0; i < converted.length; i++){
+			messagesDiv.innerHTML += "<p>" + 
+					converted[i]['content'] + " " +
+					converted[i]['username'] +
+						"</p>";
+		}	
+	}
+}
